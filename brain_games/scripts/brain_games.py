ac45060440df81c20f8278393d1d8b57eefa1caf
Artem <artxnv@gmail.com>
2024-04-10 14:50:45 +0200

    добавлен flake8 и исправлены ошибки

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+import prompt
 
-from brain_games.cli import welcome_user
 
-def main():
-    welcome_user()
+def welcome_user():
+    """
+    Приветствует пользователя и запрашивает его имя.
+    """
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
-    main()
-
-
+    welcome_user()
