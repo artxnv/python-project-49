03b8850d0457ce5f8eb7751d17dd7697ab9eed68
artxnv <artxnv@gmail.com>
2024-07-03 22:33:06 +0200

    even fix 3

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -32,7 +32,7 @@ def play_game(name):
                 f"'{user_answer}' is wrong answer ;(. "
                 f"Correct answer was '{correct_answer}'."
             )
-            print("Let's try again, {name}!")
+            print(f"Let's try again, {name}!")
             return
     print(f"Congratulations, {name}!")
 
