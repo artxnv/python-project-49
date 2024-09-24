47c2d1093ee71261eaa83e4d57e10037957fa5e1
artxnv <artxnv@gmail.com>
2024-09-24 16:16:24 +0200

    fix prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,6 +1,7 @@
 import random
 import prompt
 
+
 def is_prime(number):
     if number < 2:
         return False
@@ -9,30 +10,40 @@ def is_prime(number):
             return False
     return True
 
-def play_game():
+
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
+    return name  
+
+
+def play_game(name):  
     correct_answers_needed = 3
     correct_answers = 0
-    
-    print('Answer "yes" if given number is prime. Otherwise answer "no."')
-
     while correct_answers < correct_answers_needed:
         number = random.randint(1, 100)
-        print("Question:", number)
+        print(f"Question: {number}")
         user_answer = prompt.string("Your answer: ").lower()
         correct_answer = 'yes' if is_prime(number) else 'no'
-        
         if user_answer == correct_answer:
             print("Correct!")
             correct_answers += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+                f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+            )
             print("Let's try again!")
             return
-    print("Congratulations, you won!")
+    print(f"Congratulations, {name}!")
+
 
 def main():
-    print("Welcome to the Brain Games!")  
-    play_game() 
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    play_game(name) 
+
 
 if __name__ == "__main__":
     main()
