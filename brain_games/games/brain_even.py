4dad4e767203353a62489d273fdf67845f2beade
artxnv <artxnv@gmail.com>
2024-09-28 15:36:34 +0200

    import fix

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,5 +1,4 @@
-from utils import generate_random_number
-
+from brain_games.utils import generate_random_number  # Исправленный импорт
 
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
@@ -15,7 +14,7 @@ def generate_question():
     """
     Генерирует вопрос о четности числа и правильный ответ.
     """
-    number = generate_random_number(1, 100)
+    number = generate_random_number(1, 100)  # Используем функцию из utils.py
     question = str(number)
     correct_answer = 'yes' if is_even(number) else 'no'
     return question, correct_answer
